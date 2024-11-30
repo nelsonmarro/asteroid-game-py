05b3c8616e6cb084a4ef3c2aac02effc1eae8f68
Nelson Marro <nelsonmarro99@gmail.com>
2024-11-30 00:35:23 -0500

    initial project layout

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+
+from constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_KINDS,
+    ASTEROID_SPAWN_RATE,
+    ASTEROID_MAX_RADIUS,
+)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, (0, 255, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
