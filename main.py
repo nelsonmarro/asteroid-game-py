5698cf030e8ccd2bcdf6a6a966f94b72985f8453
Nelson Marro <nelsonmarro99@gmail.com>
2024-11-30 00:53:14 -0500

    added fps to 60

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ from constants import (
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -19,8 +23,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0, 255, 0))
+        pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        dt = (clock.tick(60)) / 1000
 
 
 if __name__ == "__main__":
